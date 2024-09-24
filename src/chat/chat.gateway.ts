6f00c93e31f53603ecc6b7dbcaef574edd207c48
Rushikesh Ghuge <rushikeshghuge993@gmail.com>
2024-09-24 10:39:10 +0530

    refactore main chat.ts

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -16,43 +16,35 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   onModuleInit() {
     console.warn('Chat Server is Up');
-
-    this.server.on('connection', (socket) => {
-      const userId = socket.handshake.query.userId as string;
-      console.log(userId, 'this is user');
-      if (userId?.length > 0 && userId !== undefined) {
-        userSocketMap[userId] = socket?.id;
-        this.server.emit('getOnlineUsers', Object.keys(userSocketMap));
-        console.error('List of online users ', userSocketMap);
-      }
-      console.log('Connected : ', socket?.id);
-      socket.on('disconnect', () => {
-        delete userSocketMap[userId];
-        console.log('Disconnected : ', socket?.id);
-      });
-    });
+    // this.server.on('connection', (socket) => {
+    //   console.log('Connected : ', socket?.id);
+    // });
   }
 
   handleConnection(socket: Socket) {
     console.log('Client connected:', socket.id);
+    const userId = socket.handshake.query.userId as string;
+    if (userId?.length > 0 && userId !== undefined) {
+      userSocketMap[userId] = socket?.id;
+      this.server.emit('getOnlineUsers', Object.keys(userSocketMap));
+    }
   }
 
   handleDisconnect(socket: Socket) {
     console.error('Client disconnected:', socket.id);
+    const userId = socket.handshake.query.userId as string;
+    delete userSocketMap[userId];
   }
 
   @SubscribeMessage('chatMessage')
   handleMessage(@MessageBody() body: any, receiverId: string) {
-    // console.log(body, receiverId);
     const socketId = this.getSocketIdUsingUserId(receiverId);
     console.log('socket id for the reciver id :::  ', receiverId);
     console.warn(userSocketMap, receiverId);
-
     this.server.to(socketId).emit('chatMessage', {
       message: 'message',
       content: body,
     });
-    // this.server.emit('chatMessage', body);
     return 'Yes';
   }
 
